8af158797f66391315da5cc2262e7c4f51ae13a8
tangbq <tangbq@tiens.com>
2018-03-16 15:45:43 +0800

    On branch 1602master  Changes to be committed: 	deleted:    admin/config-dist.php 	new file:   admin/config.php 	deleted:    config-dist.php 	new file:   config.php 	new file:   image/cache/catalog/demo/banners/qrcode_for_gh_29f75db0e2c0_430-106x106.jpg 	new file:   image/cache/catalog/demo/manufacturer/brand_1-130x100.png 	new file:   image/cache/catalog/demo/manufacturer/brand_2-130x100.png 	new file:   image/cache/catalog/demo/manufacturer/brand_3-130x100.png 	new file:   image/cache/catalog/demo/manufacturer/brand_4-130x100.png 	new file:   image/cache/catalog/demo/product/product19/product19_1-200x200.jpg 	new file:   image/cache/catalog/demo/product/product21/product21_1-200x200.jpg 	new file:   image/cache/catalog/demo/product/product_1/product1_1-200x200.jpg 	new file:   image/cache/catalog/demo/product/product_9/product9_1-200x200.jpg 	new file:   image/cache/catalog/demo/product/product_9/product9_1-228x228.jpg 	new file:   image/cache/catalog/demo/product/product_9/product9_1-47x47.jpg 	new file:   image/cache/catalog/demo/product/product_9/product9_1-500x500.jpg 	new file:   image/cache/catalog/demo/product/product_9/product9_2-500x500.jpg 	new file:   image/cache/catalog/demo/product/product_9/product9_2-74x74.jpg 	new file:   image/cache/catalog/demo/product/product_9/product9_4-500x500.jpg 	new file:   image/cache/catalog/demo/product/product_9/product9_4-74x74.jpg 	new file:   image/cache/catalog/demo/product/product_9/product9_5-500x500.jpg 	new file:   image/cache/catalog/demo/product/product_9/product9_5-74x74.jpg 	new file:   image/cache/catalog/demo/slider/slide1-1140x580.jpg 	new file:   image/cache/catalog/demo/slider/slide2-1140x580.jpg 	new file:   nginx.conf 	new file:   system/storage/cache/cache.city.689.1521188212 	new file:   system/storage/cache/cache.city.702.1521188199 	new file:   system/storage/cache/cache.city.711.1521188205 	new file:   system/storage/cache/cache.country.catalog.1521188178 	new file:   system/storage/cache/cache.currency.1521187859 	new file:   system/storage/cache/cache.district.50.1521188215 	new file:   system/storage/cache/cache.geo_zone.1521188256 	new file:   system/storage/cache/cache.language.1521187859 	new file:   system/storage/cache/cache.order_status.1.1521187827 	new file:   system/storage/cache/cache.store.1521188301 	new file:   system/storage/cache/cache.zone.44.1521188178

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://mycncart.com:10080/admin/');
+define('HTTP_CATALOG', 'http://mycncart.com:10080/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://mycncart.com:10080/admin/');
+define('HTTPS_CATALOG', 'http://mycncart.com:10080/');
+
+// DIR
+define('DIR_APPLICATION', '/work/d/code/mycncart/admin/');
+define('DIR_SYSTEM', '/work/d/code/mycncart/system/');
+define('DIR_IMAGE', '/work/d/code/mycncart/image/');
+define('DIR_LANGUAGE', '/work/d/code/mycncart/admin/language/');
+define('DIR_TEMPLATE', '/work/d/code/mycncart/admin/view/template/');
+define('DIR_CONFIG', '/work/d/code/mycncart/system/config/');
+define('DIR_CACHE', '/work/d/code/mycncart/system/storage/cache/');
+define('DIR_DOWNLOAD', '/work/d/code/mycncart/system/storage/download/');
+define('DIR_LOGS', '/work/d/code/mycncart/system/storage/logs/');
+define('DIR_MODIFICATION', '/work/d/code/mycncart/system/storage/modification/');
+define('DIR_UPLOAD', '/work/d/code/mycncart/system/storage/upload/');
+define('DIR_CATALOG', '/work/d/code/mycncart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '10.0.75.1');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'mycncart1602');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'mcc_');
